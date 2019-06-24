382d1c6f806b67197baca8bd74c389dcdcab5436
Jake Waletzko <jake.waletzko@sportsengine.com>
2019-06-24 14:35:46 -0500

    reacting

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 # typed: strict
 Rails.application.routes.draw do
+  namespace: v1, defaults: { format: :json } do
+    get 'things', to: 'things#index'
+  end
   get "welcome/index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "welcome#index"
