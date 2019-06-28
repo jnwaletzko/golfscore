8259ff19e80166ed83ede9aea8269dd7595fa396
Jake Waletzko <jake.waletzko@sportsengine.com>
2019-06-28 16:00:12 -0500

    rspec test framework and ignore frontend for rails generators

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module Golfscore
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.generators do |g|
+      g.test_framework :rspec
+      g.assets false
+      g.helper false
+      g.template_engine false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
