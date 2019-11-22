e5e1e030b61ce2123dc8556931dffb80c13a2b5e
Jake Waletzko <jake.waletzko@sportsengine.com>
2019-11-21 21:14:15 -0600

    chore: annotate models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_22_023037) do
+ActiveRecord::Schema.define(version: 2019_11_22_025331) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name", null: false
@@ -36,9 +36,9 @@ ActiveRecord::Schema.define(version: 2019_11_22_023037) do
     t.string "color", null: false
     t.string "difficulty", null: false
     t.integer "total_yardage", null: false
-    t.integer "course_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "course_id"
     t.index ["course_id"], name: "index_tees_on_course_id"
   end
 
